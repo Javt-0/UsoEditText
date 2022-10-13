da6059ca899cac152a9e8abb1bbb1de8be0de548
Javt-0 <jonathan96aguilar@gmail.com>
2022-10-13 13:14:10 +0200

    Terminado

--- app/src/main/java/com/example/usoedittext/MainActivity.java
+++ app/src/main/java/com/example/usoedittext/MainActivity.java
@@ -24,7 +24,17 @@ public class MainActivity extends AppCompatActivity {
         final Button btnCambiar = findViewById(R.id.btnCambiar);
         final Button btnMezclar = findViewById(R.id.btnMezclar);
 
+        btnJuntar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt1 = String.valueOf(cajaUno.getText());
+                String txt2 = String.valueOf(cajaDos.getText());
+                String txtFinal = txt1 + " " + txt2;
 
+                mensaje.setTextSize(20);
+                mensaje.setText(txtFinal);
+            }
+        });
 
         btnMezclar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,17 +45,21 @@ public class MainActivity extends AppCompatActivity {
                 int diferencia = 0;
                 char letra1, letra2;
 
+
                 if(txt1.length() > txt2.length()){
-                    for(int i=0; i<txt1.length() - txt2.length(); i++){
+                    diferencia = txt1.length() - txt2.length();
+                    for(int i=0; i<diferencia; i++){
                         txt2 += "*";
+                        //txt2 = txt2 + "*";
                     }
                 }else if(txt2.length() > txt1.length()){
-                    for(int i=0; i<txt2.length() - txt1.length(); i++){
+                    diferencia = txt2.length() - txt1.length();
+                    for(int i=0; i<diferencia; i++){
                         txt1 += "*";
                     }
                 }
 
-                for (int i=0; i<txt1.length() - 1; i++){
+                for (int i=0; i<txt1.length(); i++){
                     letra1 = txt1.charAt(i);
                     txtFinal += letra1;
                     letra2 = txt2.charAt(i);
@@ -56,6 +70,38 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btnCambiar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt1 = String.valueOf(cajaUno.getText());
+                String txt2 = String.valueOf(cajaDos.getText());
+                String txtFinal = txt1 + " " + txt2;
+                String txtCambio = "";
+                char caracter;
+
+                for(int i=0; i<txtFinal.length(); i++){
+                    caracter = txtFinal.charAt(i);
+                    if(caracter == 'A' || caracter == 'a'){
+                        txtCambio += "4";
+                        //txtCambio = txtCambio + "4"
+                    }else if(caracter == 'E' || caracter == 'e'){
+                        txtCambio += "3";
+                    }else if(caracter == 'I' || caracter == 'i'){
+                        txtCambio += "1";
+                    }else if(caracter == 'O' || caracter == 'o'){
+                        txtCambio += "0";
+                    }else if(caracter == 'u'){
+                        txtCambio += "v";
+                    }else if(caracter == 'U'){
+                        txtCambio += "V";
+                    }else{
+                        txtCambio += caracter;
+                    }
+                }
+                mensaje.setTextSize(20);
+                mensaje.setText(txtCambio);
+            }
+        });
 
     }
 }
